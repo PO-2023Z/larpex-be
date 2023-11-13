87f83f5d146ff2c9e0e27a72046b46a9f6158e91
jgoliszewski <jakub.goliszewski01@gmail.com>
2023-11-13 18:53:03 +0100

    add: repository

---
+++ larpex-be/larpex-events/Repository/EventsRepository.cs
@@ -0,0 +1,37 @@
+﻿using larpex_events.Domain;
+
+namespace larpex_events.Repository;
+
+public class EventsRepository : IEventsRepository
+{
+    //private readonly EventContext _eventContext;
+
+    public EventsRepository()
+    {
+    }
+
+    public void Add(Event eventObject)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Event Get(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Event> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Event eventObject)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ larpex-be/larpex-events/Repository/IEventsRepository.cs
@@ -0,0 +1,12 @@
+﻿using larpex_events.Domain;
+
+namespace larpex_events.Repository;
+
+public interface IEventsRepository
+{
+    List<Event> GetAll();
+    Event Get(Guid eventId);
+    void Add(Event eventObject);
+    void Remove(Guid eventId);
+    void Update(Event eventObject);
+}
