ddce7bf97cd908c83416088b1730313329ad791f
Nina <nina.labecka@gmail.com>
2023-11-15 18:44:44 +0100

    external payments service mock init

---
+++ larpex-be/external-payments/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace external_payments.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] WeatherForecast weatherForecast)
+        {
+            if (weatherForecast == null)
+            {
+                return BadRequest();
+            }
+
+            if (weatherForecast.TemperatureC <= 0)
+            {
+                return BadRequest();
+            }
+
+            string result = new Guid().ToString() + weatherForecast.TemperatureC.ToString();
+
+            return Ok(result);
+        }
+    }
+}
